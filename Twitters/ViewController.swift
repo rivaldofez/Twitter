e9c74bc335b79357976db6698cae483e3e5dca2d
rivaldofez <rivaldofez@gmail.com>
2023-01-31 19:05:07 +0700

    init project and remove story boards

---
+++ Twitters/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Twitters
+//
+//  Created by Rivaldo Fernandes on 31/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
