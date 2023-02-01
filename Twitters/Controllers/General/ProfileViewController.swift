d441a905bd4f48e611ebcea49504b223e200a101
rivaldofez <rivaldofez@gmail.com>
2023-02-01 22:37:30 +0700

    create profile tableviewcell

--- Twitters/Controllers/General/ProfileViewController.swift
+++ Twitters/Controllers/General/ProfileViewController.swift
@@ -8,14 +8,36 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
+    
+    private let profileTableView: UITableView = {
+        let tableView: UITableView()
+        tableView.register(TweetTableViewCell.self, forCellReuseIdentifier: TweetTableViewCell.identifier)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
         navigationItem.title = "Profile"
+        
+        view.addSubview(profileTableView)
+        
+        profileTableView.delegate = self
+        profileTableView.dataSource = self
+    }
+
+}
+
+extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
     }
     
-
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TweetTableViewCell.identifier, for: indexPath) as? TweetTableViewCell else { return UITableViewCell()}
+        
+        return cell 
+    }
 }
