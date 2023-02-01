e8df5926de2548f84709cf7c0972381c916867ce
rivaldofez <rivaldofez@gmail.com>
2023-02-01 23:08:53 +0700

    add element on header view in profile page

--- Twitters/View/ProfileTableViewHeader.swift
+++ Twitters/View/ProfileTableViewHeader.swift
@@ -8,18 +8,85 @@
 import UIKit
 
 class ProfileTableViewHeader: UIView {
+    
+    private let usernameLabel: UILabel = {
+       let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "@rivaldofez"
+        label.textColor = .secondaryLabel
+        label.font = .systemFont(ofSize: 18, weight: .regular)
+        return label
+    }()
 
+    private let displayNameLabel: UILabel = {
+       let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Rivaldo Fernandes"
+        label.font = .systemFont(ofSize: 22, weight: .bold)
+        label.textColor = .label
+        return label
+    }()
+    
+    private let profileAvatarImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.clipsToBounds = true
+        imageView.layer.masksToBounds = true
+        imageView.layer.cornerRadius = 40
+        imageView.image = UIImage(systemName: "person")
+        imageView.backgroundColor = .yellow
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
     private let profileHeaderImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFill
         imageView.image = UIImage(named: "ucl")
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        backgroundColor = .red
+        addSubview(profileHeaderImageView)
+        addSubview(profileAvatarImageView)
+        addSubview(displayNameLabel)
+        addSubview(usernameLabel)
+        
+        configureConstraints()
+    }
+    
+    private func configureConstraints(){
+        let profileHeaderImageViewConstraints = [
+            profileHeaderImageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            profileHeaderImageView.topAnchor.constraint(equalTo: topAnchor),
+            profileHeaderImageView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            profileHeaderImageView.heightAnchor.constraint(equalToConstant: 180)
+        ]
+        
+        let profileAvatarImageViewConstraints = [
+            profileAvatarImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            profileAvatarImageView.centerYAnchor.constraint(equalTo: profileHeaderImageView.bottomAnchor, constant: 10),
+            profileAvatarImageView.widthAnchor.constraint(equalToConstant: 80),
+            profileAvatarImageView.heightAnchor.constraint(equalToConstant: 80)
+        ]
+        
+        let displayNameConstraints = [
+            displayNameLabel.leadingAnchor.constraint(equalTo: profileAvatarImageView.leadingAnchor),
+            displayNameLabel.topAnchor.constraint(equalTo: profileAvatarImageView.bottomAnchor, constant: 20)
+        ]
+        
+        let usernameLabelConstraints = [
+            usernameLabel.leadingAnchor.constraint(equalTo: displayNameLabel.leadingAnchor),
+            usernameLabel.topAnchor.constraint(equalTo: displayNameLabel.bottomAnchor, constant: 5)
+        ]
+        
+        NSLayoutConstraint.activate(profileHeaderImageViewConstraints)
+        NSLayoutConstraint.activate(profileAvatarImageViewConstraints)
+        NSLayoutConstraint.activate(displayNameConstraints)
+        NSLayoutConstraint.activate(usernameLabelConstraints)
     }
     
     required init?(coder: NSCoder) {
