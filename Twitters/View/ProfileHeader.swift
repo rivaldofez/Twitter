7be260fe31cdeb799980df1982059d511c4f7ee7
rivaldofez <rivaldofez@gmail.com>
2023-02-01 22:42:30 +0700

    create header view for profile page

---
+++ Twitters/View/ProfileHeader.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileHeader.swift
+//  Twitters
+//
+//  Created by Rivaldo Fernandes on 01/02/23.
+//
+
+import UIKit
+
+class ProfileHeader: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .red
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+
+}
