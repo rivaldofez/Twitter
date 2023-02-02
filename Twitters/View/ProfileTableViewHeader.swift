0449bc09bee145d6f291a85747da13312510ba60
rivaldofez <rivaldofez@gmail.com>
2023-02-02 16:24:23 +0700

    add section in profile view controller with action

--- Twitters/View/ProfileTableViewHeader.swift
+++ Twitters/View/ProfileTableViewHeader.swift
@@ -9,6 +9,25 @@ import UIKit
 
 class ProfileTableViewHeader: UIView {
     
+    private var tabs: [UIButton] = ["Tweets", "Tweets & Replies", "Media", "Likes"].map { buttonTitle in
+        let button = UIButton(type: .system)
+        button.setTitle(buttonTitle, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 16, weight: .bold)
+        button.tintColor = .label
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }
+    
+    private lazy var sectionStackView: UIStackView = {
+       let stackView = UIStackView(arrangedSubviews: tabs)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.distribution = .equalSpacing
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+        return stackView
+        
+    }()
+    
     private let followersTextLabel: UILabel = {
         let label = UILabel()
          label.translatesAutoresizingMaskIntoConstraints = false
@@ -125,9 +144,10 @@ class ProfileTableViewHeader: UIView {
         addSubview(followingTextLabel)
         addSubview(followersAccountLabel)
         addSubview(followersTextLabel)
-
+        addSubview(sectionStackView)
         
         configureConstraints()
+        configureStackButton()
     }
     
     private func configureConstraints(){
@@ -192,7 +212,12 @@ class ProfileTableViewHeader: UIView {
         
         ]
         
-
+        let sectionStackViewConstraints = [
+            sectionStackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 25),
+            sectionStackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -25),
+            sectionStackView.topAnchor.constraint(equalTo: followingAccountLabel.bottomAnchor, constant: 5),
+            sectionStackView.heightAnchor.constraint(equalToConstant: 35)
+        ]
         
         NSLayoutConstraint.activate(profileHeaderImageViewConstraints)
         NSLayoutConstraint.activate(profileAvatarImageViewConstraints)
@@ -205,12 +230,25 @@ class ProfileTableViewHeader: UIView {
         NSLayoutConstraint.activate(followingTextLabelConstraints)
         NSLayoutConstraint.activate(followersAccountLabelConstraints)
         NSLayoutConstraint.activate(followersTextLabelConstraints)
+        NSLayoutConstraint.activate(sectionStackViewConstraints)
 
+    }
+    
+    private func configureStackButton(){
+        for (_, button) in sectionStackView.arrangedSubviews.enumerated(){
+            guard let button = button as? UIButton else { return }
+            button.addTarget(self, action: #selector(didTapTab(_:)), for: .touchUpInside)
+        }
+    }
+    
+    @objc private func didTapTab(_ sender: UIButton){
+        print(sender.titleLabel?.text ?? "")
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
     
     
 
