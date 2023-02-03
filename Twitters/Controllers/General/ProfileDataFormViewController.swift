b1fe1fe1adfba3e6bb9ebedd6983c58862143cf8
rivaldofez <rivaldofez@gmail.com>
2023-02-03 17:05:59 +0700

    add form in onboard user profile and constrained it

--- Twitters/Controllers/General/ProfileDataFormViewController.swift
+++ Twitters/Controllers/General/ProfileDataFormViewController.swift
@@ -9,6 +9,33 @@ import UIKit
 
 class ProfileDataFormViewController: UIViewController {
     
+    
+    private let displayNameTextField: UITextField = {
+        let textfield = UITextField()
+        textfield.translatesAutoresizingMaskIntoConstraints = false
+        textfield.keyboardType = .default
+        textfield.backgroundColor = .secondarySystemFill
+        textfield.leftViewMode = .always
+        textfield.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
+        textfield.layer.masksToBounds = true
+        textfield.layer.cornerRadius = 8
+        textfield.attributedPlaceholder = NSAttributedString(string: "Display Name", attributes: [NSAttributedString.Key.foregroundColor: UIColor.gray])
+        return textfield
+    }()
+    
+    private let usernameTextField: UITextField = {
+        let textfield = UITextField()
+        textfield.translatesAutoresizingMaskIntoConstraints = false
+        textfield.keyboardType = .default
+        textfield.backgroundColor = .secondarySystemFill
+        textfield.leftViewMode = .always
+        textfield.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
+        textfield.layer.masksToBounds = true
+        textfield.layer.cornerRadius = 8
+        textfield.attributedPlaceholder = NSAttributedString(string: "Username", attributes: [NSAttributedString.Key.foregroundColor: UIColor.gray])
+        return textfield
+    }()
+    
     private let scrollView: UIScrollView = {
         let scrollView = UIScrollView()
         scrollView.translatesAutoresizingMaskIntoConstraints = false
@@ -27,14 +54,47 @@ class ProfileDataFormViewController: UIViewController {
         return label
     }()
     
+    private let avatarPlaceholderImageView : UIImageView = {
+       let imageview = UIImageView()
+        imageview.translatesAutoresizingMaskIntoConstraints = false
+        imageview.clipsToBounds = true
+        imageview.layer.masksToBounds = true
+        imageview.layer.cornerRadius = 60
+        imageview.backgroundColor = .lightGray
+        imageview.image = UIImage(systemName: "camera.fill")
+        imageview.tintColor = .gray
+        imageview.isUserInteractionEnabled = true
+        imageview.contentMode = .scaleAspectFit
+        return imageview
+    }()
+    
+    private let bioTextView: UITextView = {
+        let textview = UITextView()
+        textview.translatesAutoresizingMaskIntoConstraints = false
+        textview.backgroundColor = .secondarySystemFill
+        textview.layer.masksToBounds = false
+        textview.layer.cornerRadius = 8
+        textview.textContainerInset = .init(top: 15, left: 15, bottom: 15, right: 15)
+        textview.text = "Tell the world about yourself"
+        textview.textColor = .gray
+        textview.font = .systemFont(ofSize: 16)
+        return textview
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
         view.addSubview(scrollView)
         scrollView.addSubview(hintLabel)
-    
+        scrollView.addSubview(avatarPlaceholderImageView)
+        scrollView.addSubview(displayNameTextField)
+        scrollView.addSubview(usernameTextField)
+        scrollView.addSubview(bioTextView)
+
         configureConstraints()
+        
+        isModalInPresentation = true
     }
     
     private func configureConstraints(){
@@ -47,11 +107,43 @@ class ProfileDataFormViewController: UIViewController {
         
         let hintLabelConstraints = [
             hintLabel.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
-            hintLabel.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 30),
-            
+            hintLabel.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 30)
+        ]
+        
+        let avatarPlaceholderImageViewConstraints = [
+            avatarPlaceholderImageView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
+            avatarPlaceholderImageView.heightAnchor.constraint(equalToConstant: 120),
+            avatarPlaceholderImageView.widthAnchor.constraint(equalToConstant: 120),
+            avatarPlaceholderImageView.topAnchor.constraint(equalTo: hintLabel.bottomAnchor, constant: 30)
+        ]
+        
+        let displayNameTextfieldConstraints = [
+            displayNameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            displayNameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            displayNameTextField.topAnchor.constraint(equalTo: avatarPlaceholderImageView.bottomAnchor, constant: 40),
+            displayNameTextField.heightAnchor.constraint(equalToConstant: 50)
+        ]
+        
+        let usernameTextfieldConstraints = [
+            usernameTextField.leadingAnchor.constraint(equalTo: displayNameTextField.leadingAnchor),
+            usernameTextField.trailingAnchor.constraint(equalTo: displayNameTextField.trailingAnchor),
+            usernameTextField.topAnchor.constraint(equalTo: displayNameTextField.bottomAnchor, constant: 20),
+            usernameTextField.heightAnchor.constraint(equalToConstant: 50)
+        ]
+        
+        let bioTextViewConstraints = [
+            bioTextView.leadingAnchor.constraint(equalTo: displayNameTextField.leadingAnchor),
+            bioTextView.trailingAnchor.constraint(equalTo: displayNameTextField.trailingAnchor),
+            bioTextView.topAnchor.constraint(equalTo: usernameTextField.bottomAnchor, constant: 20),
+            bioTextView.heightAnchor.constraint(equalToConstant: 150)
+        
         ]
         
         NSLayoutConstraint.activate(scrollViewConstraints)
         NSLayoutConstraint.activate(hintLabelConstraints)
+        NSLayoutConstraint.activate(avatarPlaceholderImageViewConstraints)
+        NSLayoutConstraint.activate(displayNameTextfieldConstraints)
+        NSLayoutConstraint.activate(usernameTextfieldConstraints)
+        NSLayoutConstraint.activate(bioTextViewConstraints)
     }
 }
